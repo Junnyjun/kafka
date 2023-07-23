8f100b82cb19f8bb4d9f528044a91e7ff80efc65
JunnyJun <chbe5082@naver.com>
2023-07-23 22:52:26 +0900

    init

---
+++ src/main/java/io/git/kafka/KafkaSampleApplication.java
@@ -0,0 +1,13 @@
+package io.git.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSampleApplication.class, args);
+    }
+
+}
